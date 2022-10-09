f5055b3ca928ce24ca5c3f497c6d588efe77d549
Алексей Аксёнов <Aksenov-89@yandex.ru>
2022-10-10 00:15:19 +0500

    Added admin.py in Recipes app

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Recipe, Ingredient, Favourite, Tag,
+                     IngredientInRecipe, ShoppingList)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+        'added_in_favorites'
+    )
+    readonly_fields = ('added_in_favorites',)
+    list_filter = (
+        'author',
+        'name',
+        'tags',
+    )
+
+    @admin.display(description='Добавлено в Избранные')
+    def added_in_favorites(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    list_filter = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color',
+        'slug',
+    )
+
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'recipe',
+    )
+
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'recipe',
+    )
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipe(admin.ModelAdmin):
+    list_display = (
+        'recipe',
+        'ingredient',
+        'amount',
+    )
