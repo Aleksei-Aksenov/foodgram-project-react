8c04184410a1efa3a39e3c8d61b820be23cf07f0
Алексей Аксёнов <Aksenov-89@yandex.ru>
2022-10-19 19:53:21 +0500

    Created main.yml file, Dockerfile in backend app, changed db settings from sqlite to postgress, changed reqiurements, added env.example file, changed nginx.conf

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,23 +16,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -80,20 +70,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "foodgram.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -116,9 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "users.User"
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -128,10 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
 
--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -7,9 +7,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {"name": "завтрак", "color": "#E26C2D", "slug": "breakfast"},
+            {"name": "завтрак", "color": "#90EE90", "slug": "breakfast"},
             {"name": "обед", "color": "#49B64E", "slug": "dinner"},
+            {"name": "полдник", "color": "#40E0D0", "slug": "lunch"},
             {"name": "ужин", "color": "#8775D2", "slug": "supper"},
+            {"name": "перекус", "color": "#FFD700", "slug": "snack"},
         ]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(
