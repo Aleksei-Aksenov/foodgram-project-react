fffc407131c5627be58d9be80649b82650f419ec
Алексей Аксёнов <Aksenov-89@yandex.ru>
2022-10-21 22:46:58 +0500

    Filters changed

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,44 +1,43 @@
-from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import filters
 from recipes.models import Ingredient, Recipe, Tag
 
 
-class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr="istartswith")
+class IngredientsFilter(filters.FilterSet):
+    """Фильтр ингредиентов"""
+    name = filters.CharFilter(field_name='name', lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
-        fields = ("name",)
+        fields = ('name',)
 
 
-class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
+class RecipeFilter(filters.FilterSet):
+    """Фильтр рецептов"""
+    tags = filters.filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all(),
     )
-    is_favorited = filters.BooleanFilter(method="filter_is_favorited")
-    is_in_shopping_list = filters.BooleanFilter(
-        method="filter_is_in_shopping_list"
+    author = filters.CharFilter(lookup_expr='exact')
+    is_in_shopping_cart = filters.BooleanFilter(
+        field_name='is_in_shopping_cart', method='filter'
     )
+    is_favorited = filters.BooleanFilter(
+        field_name='is_favorited', method='filter'
+    )
+
+    def filter(self, queryset, name, value):
+        """Метод фильтрации рецептов"""
+        if name == 'is_in_shopping_cart' and value:
+            queryset = queryset.filter(
+                shopping_cart__user=self.request.user
+            )
+        if name == 'is_favorited' and value:
+            queryset = queryset.filter(
+                favorite__user=self.request.user
+            )
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = (
-            "author",
-            "tags",
-            "is_favorited",
-            "is_in_shopping_list",
-        )
-
-    def filter_is_favorited(self, queryset, name, data):
-        user = self.request.user
-        if data and not user.is_anonymous:
-            return queryset.filter(favorite_recipe__user=user)
-        return queryset
-
-    def filter_is_in_shopping_list(self, queryset, name, data):
-        user = self.request.user
-        if data and not user.is_anonymous:
-            return queryset.filter(
-                shopping_list_recipe__user=user)
-        return queryset
+        fields = ('author', 'tags', 'is_in_shopping_cart', 'is_favorited',)
