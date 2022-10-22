cb166cb35f42dd311f22d35a328259416d80cf7f
Алексей Аксёнов <Aksenov-89@yandex.ru>
2022-10-22 23:05:04 +0500

    create_amount_ingredients mod

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -233,7 +233,7 @@ class RecipesWriteSerializer(serializers.ModelSerializer):
     def create_amount_ingredients(self, ingredients, recipe):
         for ingredient in ingredients:
             current_ingredient = get_object_or_404(
-                Ingredient.objects.filter(id=ingredient['id'])[:1]
+                Ingredient, pk=ingredient['id']
             )
             ingr, _ = IngredientInRecipe.objects.get_or_create(
                 ingredient=current_ingredient,
